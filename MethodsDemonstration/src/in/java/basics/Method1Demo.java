097324aec194aed5dd2e62cea75edebc7adb429d
Lavanya <Lavanya@LAPTOP-S9O0AAEE>
2021-07-20 22:58:29 +0530

    Adding 1st method program

---
+++ MethodsDemonstration/src/in/java/basics/Method1Demo.java
@@ -0,0 +1,28 @@
+// Program to demonstrate no arguments and no return value, method type
+
+package in.java.basics;
+
+public class Method1Demo {
+
+	public static void main(String[] args) {
+		
+		Method1Demo md = new Method1Demo();
+		
+		md.display();
+		md.view();
+		
+	}
+	
+	public void display() {
+		System.out.println("This is display method");
+	}
+	
+	public void show() {
+		System.out.println("This is show method");
+	}
+	
+	public void view() {
+		System.out.println("This is view method");
+	}
+
+}
