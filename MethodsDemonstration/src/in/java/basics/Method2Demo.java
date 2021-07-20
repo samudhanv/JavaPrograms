eef081bc57fb54d07664d6350232bef17cae3f9b
Lavanya <Lavanya@LAPTOP-S9O0AAEE>
2021-07-20 23:49:01 +0530

    Updated Method2Demo.java Added Method3Demo.java

--- MethodsDemonstration/src/in/java/basics/Method2Demo.java
+++ MethodsDemonstration/src/in/java/basics/Method2Demo.java
@@ -1,10 +1,27 @@
+// Program to demonstrate no arguments and with return value, method type
+
 package in.java.basics;
 
 public class Method2Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Method2Demo method2Demo = new Method2Demo();
+		
+		method2Demo.getValue();	//Calling getValue method
+		System.out.println(method2Demo.getName());
+		
 
+	}
+	
+	public String getName() {
+		String name = "Java World";
+		return "Hello "+name;
+	}
+	
+	public float getValue() {
+		float val = 25.75f;
+		return val+10;
 	}
 
 }
